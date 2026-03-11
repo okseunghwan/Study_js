11c3fdcce6083d315c2175d37b3a11a2ba2a3e83
okseunghwan <pokd7004@naver.com>
2026-03-11 23:57:38 +0900

    study: document object 기초 학습

---
+++ document_object/js/script.js
@@ -0,0 +1,19 @@
+// js를 사용하는 이유는 html과 상호작용하기 위해서이다.
+// HTML의 Element들을 JavaScript를 통해 변경하고 읽을수 있다.
+
+// console에 document를 입력하면, 작성한 HTML을 가져올수 있다.
+
+
+// console에 document.title을 입력하면 HTML title항목이 나온다. 
+
+// object에서 property를 가져올수 있고, 변경할수있듯이
+
+// console에서 document.title = "hi"를 쓰고 엔터를 누르면 title이 hi로 변경된다.
+
+
+document.title = "Hello ! from JS"
+
+// html에서 title을 hello ! from html
+// js에서 document.title을 hello from js 라고 입력하면
+
+// hello! from js가 화면에 나온다.
